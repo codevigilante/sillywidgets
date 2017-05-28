6ab6d1d6dfe719e3d2ae8a99edf8f4594b72b41a
A-ron <codevigilante@gmail.com>
2017-05-28 14:53:43 -0500

    v0.2: polish mac publish script;

--- silly/publish_mac_10.11.sh
+++ silly/publish_mac_10.11.sh
@@ -4,13 +4,16 @@ set -e
 
 BUILD_CMD="dotnet publish -c Release -r osx.10.11-x64"
 MAC_INSTALL="cp ./installscripts/install_mac.sh ./bin/Release/netcoreapp1.1/osx.10.11-x64/install.sh"
-ARCHIVE_CMD="zip -r ./bin/Release/netcoreapp1.1/osx.10.11-x64/sillywidgets-v0.2.zip ./bin/Release/netcoreapp1.1/osx.10.11-x64/"
+CD_CMD="cd bin/Release/netcoreapp1.1/osx.10.11-x64"
+ARCHIVE_CMD="zip -r -X ../../sillywidgets-v0.2-osx10.11.zip *"
 
 echo "-----------------------------------------------------"
 echo "$BUILD_CMD"
 $BUILD_CMD
 echo "$MAC_INSTALL"
 $MAC_INSTALL
+echo "$CD_CMD"
+$CD_CMD
 echo "$ARCHIVE_CMD"
 $ARCHIVE_CMD
 echo "-----------------------------------------------------"
